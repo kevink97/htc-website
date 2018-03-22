35f16e6497588245db985e1571512de8d5fbc65b
Kevin Kang <kevink97@cs.washington.edu>
2018-03-22 14:11:34 -0700

    initial commit: Club Dues

---
+++ js/dues.js
@@ -0,0 +1,21 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyAJGFuMK-DhPuTgd61HLL47RMtcmXX7ckM",
+    authDomain: "uw-htc-website.firebaseapp.com",
+    databaseURL: "https://uw-htc-website.firebaseio.com",
+    projectId: "uw-htc-website",
+    storageBucket: "uw-htc-website.appspot.com",
+    messagingSenderId: "771697394602"
+  };
+  firebase.initializeApp(config);
+  
+  firebase.auth().onAuthStateChanged(function(user) {
+    if (user) {
+        // TODO: implement when user is logged in
+    }
+    else {
+        // TODO: implement when user is logged off
+    }
+  })
+  
+  
